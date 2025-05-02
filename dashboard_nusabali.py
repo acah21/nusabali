7bf185a5ff2d6b4d0189a1ed23321d7d582ada85
acah21 <iamacah21@gmail.com>
2025-05-02 16:41:45 +0700

    Update dashboard_nusabali.py

--- dashboard_nusabali.py
+++ dashboard_nusabali.py
@@ -7,8 +7,6 @@ Original file is located at
     https://colab.research.google.com/drive/1yVQ_7fhxeeUW_0O6Z4OBkLUrmRc7sf1R
 """
 
-pip install streamlit pandas
-
 import streamlit as st
 import pandas as pd
 
